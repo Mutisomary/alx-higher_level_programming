45ce1359a20897accf6d365d4661a3b3b0e18975
Mutisomary <marymutso2016@gmail.com>
2023-06-12 05:46:00 -0700

    corrent indentation

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
-     copy = [num for num in my_string if num != 'c' and num != 'C']
-     return ("".join(copy))
+    """Remove all characters c and C from a string."""
+    copy = [num for num in my_string if num != 'c' and num != 'C']
+    return ("".join(copy))

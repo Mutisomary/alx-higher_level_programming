ae216de7d8dac56ab3bba59eb57c35a2276f51f8
Mutisomary <marymutso2016@gmail.com>
2023-06-26 18:29:36 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             printed += 1
     except IndexError:
         pass
-    finally:
-        print()
-        return printed
+        
+    print()
+    return printed
 

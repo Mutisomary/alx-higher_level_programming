7082d44dcf3e901c6dd2e3185447a98973180d6a
Mutisomary <marymutso2016@gmail.com>
2023-07-03 18:55:39 -0700

    A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     """rectangle class"""
 
     number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.height = height
@@ -59,7 +60,7 @@ class Rectangle:
 
         if self.__width > 0 and self.__height > 0:
             for y in range(self.__height):
-                rectangle += '#' * self.__width + '\n'
+                rectangle += str(self.print_symbol) * self.__width + '\n'
 
         return rectangle[:-1]
 
@@ -71,6 +72,18 @@ class Rectangle:
         """Sets the del behavior of the Rectangle object."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
 
     @classmethod
     def square(cls, size=0):

1674c824bb89d3ac63230837b45c4e57acf09cb8
Mutisomary <marymutso2016@gmail.com>
2023-07-10 07:49:24 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""6-base_geometry module"""
+
+
+class BaseGeometry:
+    """Defining a base geometry class"""
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")

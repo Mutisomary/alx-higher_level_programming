dab7ca557f4a56dc5b1f08689d4cdfd4fd44cd42
Mutisomary <marymutso2016@gmail.com>
2023-07-10 07:17:43 -0700

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a class checking function"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if an object is an instance of a class
+    Return True or False
+    """
+    if type(object) == a_class:
+        return True
+    else:
+        return False

d4cd5b6cca29c2f0c2bce7653a07b4bcedf3001b
Mutisomary <marymutso2016@gmail.com>
2023-07-20 07:06:19 -0700

    base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
-import json
-import os
-import csv
+"""Write the first class Base"""
 
-"""Defining a base class"""
+import json
+import csv
+import turtle
 
 
 class Base:
-    """The base class"""
+    """The base(super) class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializing the base class"""
+        """Initializing the base instance"""
         if id is not None:
             self.id = id
         else:
@@ -64,3 +64,87 @@ class Base:
                 return [cls.create(**dic) for dic in list]
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """saves a list of objects to a CSV file"""
+        filename = cls.__name__ + ".csv"  # creates a filename for the CSV file
+        with open(filename, "w", newline="") as file:
+            if list_objs is None or list_objs == []:
+                file.write("[]")  # Return an empty list
+            else:
+                if cls.__name__ == "Rectangle":
+                    # assign fieldnames depending on the object
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+
+                # write dictionaries to the CSV file
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                for obj in list_objs:
+                    # converts the object to a dictionary
+                    writer.writerow(obj.to_dictionary())
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """reads data from a CSV file and
+        returns a list of instantiated classes
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as file:
+                if cls.__name__ == "Rectangle":
+                    # Depending on the class name, different fieldnames
+                    #  will be used for reading data from the CSV file
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                # reads data from the CSV file
+                list = csv.DictReader(file, fieldnames=fieldnames)
+
+                # convert the read dictionaries into a list of dictionaries
+                list = [dict([k, int(v)] for k, v in d.items())
+                        for d in list]
+                return [cls.create(**d) for d in list]
+        except IOError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw Rectangles and Squares using the turtle module.
+        Args:
+            list_rectangles (list): A list of Rectangle objects to draw.
+            list_squares (list): A list of Square objects to draw.
+        """
+        turt = turtle.Turtle()
+        turt.screen.bgcolor("#b7312c")
+        turt.pensize(3)
+        turt.shape("turtle")
+
+        turt.color("#ffffff")
+        for rect in list_rectangles:
+            turt.showturtle()
+            turt.up()
+            turt.goto(rect.x, rect.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(rect.width)
+                turt.left(90)
+                turt.forward(rect.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for sq in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+            for i in range(2):
+                turt.forward(sq.width)
+                turt.left(90)
+                turt.forward(sq.height)
+                turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()

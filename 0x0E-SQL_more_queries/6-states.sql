a421b452aeefd65be043269543f701d3b022f3cd
Mutisomary <marymutso2016@gmail.com>
2023-09-13 03:50:19 -0700

    commit

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- creates the database hbtn_0d_usa
 -- create the table states on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT AUTO INCREMENT PRIMARY KEY NOT NULL,
 	NAME VARCHAR(256) NOT NULL
 );

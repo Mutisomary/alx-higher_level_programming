7efa1ad073582b3a0e8ce13d138433f0efb615d6
Mutisomary <marymutso2016@gmail.com>
2023-09-29 13:24:26 -0700

    correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     # display response body
     print("Body response:")
     print("\t- type: {}".format(type(response.content)))
-    print("\t- content: {}".format( response.content.decode('utf-8')))
+    print("\t- content: {}".format(response.content))

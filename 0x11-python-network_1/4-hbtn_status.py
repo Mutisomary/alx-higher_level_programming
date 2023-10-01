607f52aa68be8dcbb4969583df457bdcd04179e3
Mutisomary <marymutso2016@gmail.com>
2023-10-01 11:35:03 -0700

    what's my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
     # display response body
     print("Body response:")
-    print("\t- type: {}".format(type(response.content)))
-    print("\t- content: {}".format(response.content.decode('utf-8')))
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))

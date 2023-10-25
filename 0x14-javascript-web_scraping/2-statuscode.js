850765a539f79eeeeb78a1f145f7717bb2da66ab
Mutisomary <marymutso2016@gmail.com>
2023-10-25 04:40:06 -0700

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-//script that display the status code of a GET request
+// script that display the status code of a GET request
 const request = require('request');
 const url = process.argv[2];
 
